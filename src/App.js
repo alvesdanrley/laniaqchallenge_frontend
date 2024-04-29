4a49120117123962548b8097a430366a7b6ab048
Danrley Alves <danrleyalves.ufrn@gmail.com>
2024-04-29 20:59:29 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import GlobalStyle from "./styles/global"
+import styled from "styled-components"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Form from "./components/Form"
+import Grid from "./components/Grid"
+import axios from "axios"
+
+const Container = styled.div `
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2``
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [onEdit, setOnEdit] = useState(null)
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800")
+      setUsers(res.data.sort((a,b) => (a.nome > b.nome ? 1 : -1)))
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [setUsers])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container>
+      <Title>USUÁRIOS</Title>
+      <Form getUsers={getUsers} onEdit={onEdit} setOnEdit={setOnEdit} />
+      <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit}/>
+    </Container>
+    <ToastContainer autoClose={3000} />
+    <GlobalStyle />
+    </>
   );
 }
 
-export default App;
+export default App
